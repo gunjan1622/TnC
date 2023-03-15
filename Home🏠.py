da90747069c57745b7056c13cff3989540728fa1
gunjan1622 <janguntayal02@gmail.com>
2023-03-15 10:08:47 +0530

    updated

--- Home🏠.py
+++ Home🏠.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="The Fine Art of Summarization",
 )
 st.sidebar.subheader("Made with ❤️ by [Gunjan Agrawal](https://github.com/gunjan1622) and [Hemanth Sai](https.github.com/HemanthSai7)")
-st.sidebar.markdown("### *Summarizer, a project of my bestie Gunjuuuuu*")
+#st.sidebar.markdown("### *Summarizer, a project of my bestie Gunjuuuuu*")
 
 st.title("The Fine Art of Summarization")
 
