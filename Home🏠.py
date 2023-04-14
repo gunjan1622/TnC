ab77dba12b81136509cef2cd6666456f6dddeca8
gunjan1622 <janguntayal02@gmail.com>
2023-04-14 18:44:23 +0530

    Home Page

--- Home🏠.py
+++ Home🏠.py
@@ -1,13 +1,33 @@
 import streamlit as st
 
+
 st.set_page_config(
-    page_title="The Fine Art of Summarization",
+    page_title="TermsBrief",
 )
 st.sidebar.subheader("Made with ❤️ by [Gunjan Agrawal](https://github.com/gunjan1622) and [Hemanth Sai](https.github.com/HemanthSai7)")
 #st.sidebar.markdown("### *Summarizer, a project of my bestie Gunjuuuuu*")
+st.header('TERMSBRIEF')
+st.markdown('Tired of reading those endless **:orange[terms and conditions]** before agreeing to them?')
+#st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+col1, col2 = st.columns([1.5, 1])
+with col1:
+    st.markdown('Agreeing to **:green[terms and conditions]** with your eyes closed?')
+with col2:
+    st.image('https://media.tenor.com/TatG54muuNYAAAAi/eyes-looking.gif', width=47)
 
-st.title("The Fine Art of Summarization")
+st.markdown('Without thinking how someone can be stealing your personal information.')
 
+_left, mid, _right = st.columns([0.3, 1, 0.1])
+with mid:
+    st.image('https://media.tenor.com/ZWHGB0dc7koAAAAi/theumbrellaacademy-theumbrellaacademyhazelandchacha.gif', width=170)
 
+col1, col2, col3= st.columns([0.5, 1.5, 0.5])
+with col1:
+    st.image('https://media.tenor.com/6ceOmdT7SHkAAAAi/emoji-emojis.gif', width=47)
+with col2:
+    st.markdown('**:orange[Termsbreif  is the perfect tool for making quick, simple summaries of important legal documents.]**')
+with col3:
+    st.image('https://media.tenor.com/6ceOmdT7SHkAAAAi/emoji-emojis.gif', width=47)
 
-        
+st.markdown('**:white[Simply  paste your text in the given area and choose the summary option u want,  to create your own summary in seconds. And Voila, u protected urself from someone else governing your personal information.]**')
+
--- SummaryAPI/utils/DBConnection.py
+++ SummaryAPI/utils/DBConnection.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from snowflake.snowpark import Row
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
+# from snowflake.snowpark import Row
+# from snowflake.snowpark.session import Session
+# from snowflake.snowpark.functions import avg, sum, col,lit
 
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
@@ -21,9 +21,9 @@ class DBConnection:
            Exception: It raises an exception if the client instance is not created.
     """
     __client = None #This is the client variable that is used to connect to the database
-    __session = None #This is the engine variable that is used to connect to the database
+    # __session = None #This is the engine variable that is used to connect to the database
     flag_client = False
-    flag_session=False
+    # flag_session=False
 
     try:
         # Authenticate the client using your key and endpoint 
@@ -37,21 +37,21 @@ class DBConnection:
                     credential=ta_credential)
             return text_analytics_client
         
-        @staticmethod
-        def authenticate_SF_session():
-            # Snowflake database credentials
-            connection_parameters={
-            "user" : os.environ["SF_USER"],
-            "password" : os.environ["SF_PASSWORD"],
-            "account" : os.environ["SF_ACCOUNT"],
-            "database" : os.environ["SF_DATABASE"],
-            "schema" : os.environ["SF_SCHEMA"],
-            "warehouse" : os.environ["SF_WAREHOUSE"],
-            "role":os.environ["SF_ROLE"],
-            }
-            session = Session.builder.configs(connection_parameters).create()
-            # print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
-            return session
+        # @staticmethod
+        # def authenticate_SF_session():
+        #     # Snowflake database credentials
+        #     connection_parameters={
+        #     "user" : os.environ["SF_USER"],
+        #     "password" : os.environ["SF_PASSWORD"],
+        #     "account" : os.environ["SF_ACCOUNT"],
+        #     "database" : os.environ["SF_DATABASE"],
+        #     "schema" : os.environ["SF_SCHEMA"],
+        #     "warehouse" : os.environ["SF_WAREHOUSE"],
+        #     "role":os.environ["SF_ROLE"],
+        #     }
+        #     session = Session.builder.configs(connection_parameters).create()
+        #     # print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
+        #     return session
         
     except Exception as e:
         raise Exception(f"Error while authenticating the client: {e}")
@@ -68,11 +68,11 @@ class DBConnection:
             DBConnection.__client = DBConnection.authenticate_client()
             DBConnection.flag_client = True
 
-        if DBConnection.__session is not None:
-            raise Exception("This class is a singleton!")
-        else:
-            DBConnection.__session = DBConnection.authenticate_SF_session()
-            DBConnection.flag_session = True
+        # if DBConnection.__session is not None:
+        #     raise Exception("This class is a singleton!")
+        # else:
+        #     DBConnection.__session = DBConnection.authenticate_SF_session()
+        #     DBConnection.flag_session = True
             
 
     @staticmethod  # A static method is a method that is called without creating an instance of the class.
@@ -84,14 +84,14 @@ class DBConnection:
         """
         return DBConnection.__client
     
-    @staticmethod
-    def get_engine():
-        """The get_engine() function is used to get the engine instance.
+    # @staticmethod
+    # def get_engine():
+    #     """The get_engine() function is used to get the engine instance.
 
-        Returns:
-            DBConnection.__session: It returns the engine instance.
-        """
-        return DBConnection.__session
+    #     Returns:
+    #         DBConnection.__session: It returns the engine instance.
+    #     """
+    #     return DBConnection.__session
 
 
 
